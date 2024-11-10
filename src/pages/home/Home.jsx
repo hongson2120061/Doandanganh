8003d25e0fa2a51e25685b09adceea3e424d9336
Hong Son <hongson2120061@gmai.com>
2024-11-10 09:48:53 +0700

    version 2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
 export const HomePage = () => {
     return (
         <>
-            <div  className = "bg-[#aaffaa]" >
-                Dâu tây đỏ thắm, chăm khéo tay, ngọt lành trĩu quả!
+            <div  className = "w-[100%] h-[580px] flex items-center justify-center text-[60px] font-[700] text-[#0dbf66]  bg-[#f3c3c3]" >
+                <div>
+                    Chào mừng bạn đến với XFARM!
+                </div>                
             </div>
         </>
     )
